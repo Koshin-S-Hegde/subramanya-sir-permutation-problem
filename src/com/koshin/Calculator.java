5ac4f976ef0e2a95135dc47c44c9d8badcc2080e
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-11-17 21:55:58 +0530

    Added a check for if sir's conditions.

--- src/com/koshin/Calculator.java
+++ src/com/koshin/Calculator.java
@@ -1,6 +1,26 @@
 package com.koshin;
 
 public class Calculator {
+    private boolean isConditionMet(int number) {
+
+        for (int previousDigit = number % 100; number > 0; number /= 10) {  // Iterating through it backwards
+            int currentDigit = number % 10;
+            if (
+                    currentDigit > 6 ||
+                    currentDigit < 1 ||
+                    previousDigit == currentDigit + 1
+            )
+                return false;
+            previousDigit = currentDigit;
+        }
+
+        return true;
+    }
+
+    private Calculator() {
+    }
+
     public static void main(String[] args) {
+        new Calculator();
     }
 }
